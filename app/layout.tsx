bbec772cf020f7781eda3a715c0c4405edaec3af
Christian F. Brinkmann <christian.brinkmann09@gmail.com>
2026-05-25 17:36:13 +0200

    perf: remove bloated 1.3MB favicon.svg and optimize icon metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,6 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       { url: "/favicon-96x96.png", sizes: "96x96", type: "image/png" },
-      { url: "/favicon.svg", type: "image/svg+xml" },
     ],
     shortcut: "/favicon.ico",
     apple: [
